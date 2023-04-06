1f7e3d8478f302ab6e88a8d81d921905ac4c3288
Natnael-101 <natiy1359@gmail.com>
2023-04-06 11:54:41 -0700

    fix task 6

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,9 +1,9 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
-  * get_endianness - returns processor endianness
-  * Return: 0 if big endian, 1 if little endian
-  */
+ * get_endianness - returns processor endianness
+ * Return: 0 if big endian, 1 if little endian
+ */
 int get_endianness(void)
 {
 	unsigned int num;
